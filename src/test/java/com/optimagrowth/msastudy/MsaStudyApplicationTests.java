9039abba210303e0537e19846c414ba87700d3c3
준우 이 <gs04043@naver.com>
2024-01-13 17:53:48 +0900

    Initial commit

---
+++ src/test/java/com/optimagrowth/msastudy/MsaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.msastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
